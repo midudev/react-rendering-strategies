458fb6304095eb2d59041837e4a7463a3058f8b8
midudev <miduga@gmail.com>
2019-08-22 19:46:41 +0200

    Hydrate content instead render the content

--- strategies/react-progressive-hydration/index.js
+++ strategies/react-progressive-hydration/index.js
@@ -1,23 +1,37 @@
+import React, {useEffect, useRef} from 'react'
+import ReactDOM from 'react-dom'
 import {useNearScreen} from '../useNearScreen'
 
-const {useRef} = React
+const EMPTY_HTML = {__html: ''}
+const isServer = typeof window === 'undefined'
 
 export default function ProgressiveHydration({children, force}) {
   const ref = useRef(null)
-  const render = typeof window === 'undefined' || force
   const isNearScreen = useNearScreen({ref})
 
-  // we're in the server or the element is above the fold
-  if (render || isNearScreen) {
-    return <div>{children}</div>
+  useEffect(
+    function() {
+      // CLIENT:
+      // If we want to force the hydration or the element is near screen
+      // then we hydrate the content to get the functionality ready
+      if (force || isNearScreen) {
+        ReactDOM.hydrate(children, ref.current)
+      }
+    },
+    [children, force, isNearScreen]
+  )
+
+  // SERVER: Just render the content as usual
+  if (isServer) {
+    return <div ref={ref}>{children}</div>
   }
 
-  // avoid hydration until is in the viewport
+  // CLIENT: Avoid hydration until we say so
   return (
     <div
       ref={ref}
       suppressHydrationWarning
-      dangerouslySetInnerHTML={{__html: ''}}
+      dangerouslySetInnerHTML={EMPTY_HTML}
     />
   )
 }
