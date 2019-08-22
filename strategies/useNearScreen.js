8a7f03e57c83665ce057dd3bc536660042622b86
midudev <miduga@gmail.com>
2019-08-22 19:46:26 +0200

    Clean effect when unmounted and add options

--- strategies/useNearScreen.js
+++ strategies/useNearScreen.js
@@ -1,26 +1,58 @@
 import {useState, useEffect} from 'react'
 
+const isClient = typeof window !== 'undefined'
+const hasIntersectionObserverSupport = () =>
+  typeof window.IntersectionObserver !== 'undefined'
+
+// import intersection observer polyfill only if on client
+// and if not supported by browser
+const getIntersectionObserver = () =>
+  Promise.resolve(
+    isClient &&
+      !hasIntersectionObserverSupport() &&
+      import('intersection-observer')
+  )
+
+// options for intersection observer in order to improve the effect
+const intersectionObserverOptions = {
+  root: null, // windows viewport will be used
+  rootMargin: '200px 0px 0px 0px'
+}
+
+const handleIntersect = (entries, observer) => {
+  entries
+    .filter(entry => entry.isIntersecting)
+    .forEach(entry => {
+      const {target} = entry
+      target._onIntersect(observer)
+    })
+}
+
+// create observer instance
+const observer = getIntersectionObserver().then(
+  () =>
+    new window.IntersectionObserver(
+      handleIntersect,
+      intersectionObserverOptions
+    )
+)
+
 export const useNearScreen = ({ref}) => {
   const [show, setShow] = useState(false)
 
   useEffect(
     function() {
-      if (!ref || !ref.current) return
+      const {current} = ref
+      if (!current) return
 
-      Promise.resolve(
-        typeof window.IntersectionObserver !== 'undefined'
-          ? window.IntersectionObserver
-          : import('intersection-observer')
-      ).then(() => {
-        const observer = new window.IntersectionObserver(function(entries) {
-          const {isIntersecting} = entries[0]
-          if (isIntersecting) {
-            setShow(true)
-            observer.disconnect()
-          }
-        })
-        observer.observe(ref.current)
-      })
+      observer.then(observer => observer.observe(current))
+
+      current._onIntersect = observer => {
+        setShow(true)
+        observer.unobserve(current)
+      }
+      // clean observed element when unmounted
+      return () => (current._isSubscribed = false)
     },
     [ref]
   )
