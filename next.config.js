6614531c06f01cf35728bb83cf8a80fb181d024d
midudev <miduga@gmail.com>
2019-10-07 15:45:52 +0200

    Fix linter

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const path = require('path')
 
 module.exports = {
   webpack(config, options) {
-    config.resolve.alias['components'] = path.join(__dirname, 'components')
-    config.resolve.alias['getNews'] = path.join(__dirname, 'data/getNews.js')
+    config.resolve.alias.components = path.join(__dirname, 'components')
+    config.resolve.alias.getNews = path.join(__dirname, 'data/getNews.js')
     config.resolve.alias['@midudev'] = path.join(__dirname, 'strategies')
 
     return config
