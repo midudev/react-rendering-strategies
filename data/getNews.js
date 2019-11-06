5884ba56cc88a55f2823ed13e194a38911bb1a33
midudev <miduga@gmail.com>
2019-11-06 15:16:12 +0100

    Use local or production depending on node_env

--- data/getNews.js
+++ data/getNews.js
@@ -1,7 +1,12 @@
 import fetch from 'isomorphic-unfetch'
 
+const URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://react-rendering.midudev.now.sh/'
+    : 'http://localhost:3000/'
+
 export default () => {
-  return fetch('https://react-rendering.midudev.now.sh/api/get-news')
+  return fetch(`${URL}/api/get-news`)
     .then(res => res.json())
     .catch(() => import('./news.js').then(imported => imported.default))
 }
